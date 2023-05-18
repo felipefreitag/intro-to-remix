efa50d9d472951f3b41bcc103fe64e5de82ea4f8
Felipe Freitag Vargas <ffvargas@gmail.com>
2023-05-18 15:52:43 -0300

    Show create button only if logged in

--- app/routes/_main.barks.tsx
+++ app/routes/_main.barks.tsx
@@ -1,12 +1,16 @@
+import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { Link, useLoaderData, type V2_MetaFunction } from '@remix-run/react'
 import { db } from '~/db.server'
+import { getUser } from '~/session.server'
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'InstaDog' }, { description: 'O app para doguitos' }]
 }
 
-export async function loader() {
+export async function loader({ request, params }: LoaderArgs) {
+  const user = await getUser(request)
+
   const barks = await db.bark.findMany({
     include: {
       author: true,
@@ -15,38 +19,42 @@ export async function loader() {
       createdAt: 'desc',
     },
   })
-  return json({ barks })
+  return json({ barks, user })
 }
 
 export default function Index() {
   const data = useLoaderData<typeof loader>()
 
+  const { user, barks } = data
+
   return (
     <>
       <div className="mb-10 flex justify-between">
         <h1 className="text-bold text-lg">Latidos</h1>
-        <Link
-          to={`/barks/new`}
-          className="rounded-full bg-indigo-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 enabled:hover:bg-indigo-800 disabled:opacity-50"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
+        {user && (
+          <Link
+            to={`/barks/new`}
+            className="rounded-full bg-indigo-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 enabled:hover:bg-indigo-800"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-            />
-          </svg>
-        </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+              />
+            </svg>
+          </Link>
+        )}
       </div>
-      <ul className="divide-y divide-gray-100">
-        {data.barks.map((bark) => (
+      <ul className="divide-y divide-gray-100 overflow-y-scroll">
+        {barks.map((bark) => (
           <li
             key={bark.author.email}
             className="flex items-start justify-between gap-x-6 py-5"
