4be0d6f7e1b48d7ec224a103385fec8bef2205cc
Felipe Freitag Vargas <ffvargas@gmail.com>
2023-06-01 13:34:40 -0300

    Add presentation nav

--- app/routes/presentation.tsx
+++ app/routes/presentation.tsx
@@ -1,7 +1,29 @@
-import { Outlet } from '@remix-run/react'
+import { Outlet, useMatches, useNavigate } from '@remix-run/react'
 
-export default () => (
-  <div className="relative min-h-screen">
-    <Outlet />
-  </div>
-)
+export default () => {
+  const navigate = useNavigate()
+  const match = useMatches()
+  const [currentRoute] = match.slice(-1)
+  const currentIndex = currentRoute.id.split('.')[1]
+  const MIN_SLIDE = 1
+  const MAX_SLIDE = 2
+  const prevIndex = Math.max(Number(currentIndex) - 1, MIN_SLIDE)
+  const nextIndex = Math.min(Number(currentIndex) + 1, MAX_SLIDE)
+
+  return (
+    <div
+      tabIndex={0}
+      aria-label="Left navigates back, right navigates to the next slide"
+      onKeyDown={(event) => {
+        if (event.key === 'ArrowLeft') {
+          navigate(`/presentation/${prevIndex}`)
+        } else if (event.key === 'ArrowRight') {
+          navigate(`/presentation/${nextIndex}`)
+        } else return
+      }}
+      className="relative min-h-screen bg-white"
+    >
+      <Outlet />
+    </div>
+  )
+}
